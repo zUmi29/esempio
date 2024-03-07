91f93f724b8df30ab4f9cca80fc7569fc6e88ce0
Manuel Marteddu <manuelmarteddu11@gmail.com>
2024-03-07 17:27:17 +0100

    Initial commit

---
+++ src/main/java/esempio/git/esempio/EsempioApplication.java
@@ -0,0 +1,13 @@
+package esempio.git.esempio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsempioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsempioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esempio/git/esempio/EsempioApplicationTests.java
@@ -0,0 +1,13 @@
+package esempio.git.esempio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
